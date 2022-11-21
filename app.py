b8047792c128aa25778f9c35a12ff26fa80efb3a
Andrew-715 <kutuzkin98@mail.ru>
2022-11-21 14:05:35 +0800

    complete homework

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import utils
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+"""
+Создаём вьюшку для отображения главной страницы.
+"""
 
 
 @app.route('/')
@@ -10,17 +16,72 @@ def home_page():
     return 'Это главная страница'
 
 
+"""
+Создаём вьюшку для отображения фильма по названию.
+"""
+
+
 @app.route('/movie/<title>')
 def info_about_movie(title):
-
     result = utils.search_by_name(title)
     return jsonify(result)
 
-#
-# @app.route('/movie/<year>/to/<year>')
-# def range_release_years():
-#     pass
+
+"""
+Создаём вьюшку для отображения фильма по годам.
+"""
+
+
+@app.route('/movie/<int:old_year>/to/<int:new_year>')
+def range_release_years(old_year, new_year):
+    result =  utils.search_by_release_year(old_year, new_year)
+    return jsonify(result)
+
+
+"""
+Создаём вьюшку для отображения фильмов по детскому рейтингу.
+"""
+
+
+@app.route('/movie/rating/children')
+def search_movie_rating_children():
+    result = utils.movie_rating_children()
+    return jsonify(result)
+
+
+"""
+Создаём вьюшку для отображения фильмов по семейному рейтингу.
+"""
+
+
+@app.route('/movie/rating/family')
+def search_movie_rating_family():
+    result = utils.movie_rating_family()
+    return jsonify(result)
+
+
+"""
+Создаём вьюшку для отображения фильмов по взрослому рейтингу.
+"""
+
+
+@app.route('/movie/rating/adult')
+def search_movie_rating_adult():
+    result = utils.movie_rating_adult()
+    return jsonify(result)
+
+
+"""
+Создаём вьюшку для отображения фильмов по жанру.
+"""
+
+
+@app.route('/movie/genre/<genre>')
+def search_movie_genre(genre):
+    result = utils.title_genre(genre)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
+
--- utils.py
+++ utils.py
@@ -1,22 +1,29 @@
 import sqlite3
 
+
 """
-Создаём функцию с SQL-запросом и выводим название, страну, год релиза, жанр
-и описание фильма.
+Создаём функцию для превращения кортежи в список словарей.
 """
 
 
 def convert_tuple_to_list_of_dicts(result_all, keys):
+    result = []
+
     """
     С помощью циклов for добавляем необходимые значения к списку ключей
     и добавляем это в список result.
     """
-    result = []
 
     for result_row in result_all:
         result.append({keys[i]: result_row[i] for i in range(len(keys))})
 
     return result
+
+
+"""
+Создаём функцию с SQL-запросом и выводим название, страну, год релиза, жанр
+и описание фильма.
+"""
 
 
 def search_by_name(movie_name):
@@ -26,7 +33,7 @@ def search_by_name(movie_name):
         query = """SELECT title, country, release_year, listed_in, description
                  FROM netflix
                  WHERE title LIKE :title
-                 AND type = 'Movie'
+                 AND country !=''
                  LIMIT 1
         """
 
@@ -35,7 +42,7 @@ def search_by_name(movie_name):
         result_all = cursor.fetchall()
 
         # Прописываем список ключей, которые нам нужны.
-        keys = ["title",  "country", "release_year", "listed_in", "description" ]
+        keys = ["title", "country", "release_year", "listed_in", "description"]
 
         # Превращаем кортежи в список словарей
         return convert_tuple_to_list_of_dicts(result_all, keys)[0]
@@ -46,126 +53,256 @@ def search_by_name(movie_name):
 """
 
 
-def search_by_release_year():
+def search_by_release_year(old_year, new_year):
+
     with sqlite3.connect('netflix.db') as connection:
+        connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
+        query = """
+                SELECT title, release_year
+                FROM netflix
+                WHERE release_year BETWEEN @old_year AND @new_year
+                LIMIT 100
+                """
 
-        querly = """
-            SELECT title, release_year
-            FROM netflix
-            WHERE "type" = 'Movie'
-            LIMIT 100
+        cursor.execute(query, (old_year, new_year))
+        sql_result = cursor.fetchall()
+
+        """
+        Создаём пустой список, который будет содержать в себе словари.
         """
 
-        result_all = cursor.execute(querly)
-        """
-        Прописываем список ключей, которые нам нужны.
-        """
-        keys = [
-            "title",
-            "release_year"
-        ]
-        """
-        Создаём пустой список, который будет в себе содержать ключи и значения.
-        """
-        result = []
-        """
-        С помощью циклов for добавляем необходимые значения к списку ключей
-        и добавляем это в список result.
-        """
-        for result_row in result_all:
-            result.append({keys[i]: result_row[i] for i in range(len(keys))})
+        sql_year_dict = []
 
-        return result
+        """
+        С помощью циклов for задаём и добавляем необходимые значения к списку ключей
+        и добавляем это в список sql_year_dict.
+        """
+
+        for i in sql_result:
+            title = i["title"]
+            release_year = i["release_year"]
+
+            sql_year_dict.append(
+                {
+                    "title": title,
+                    "release_year": release_year
+                }
+            )
+
+    return sql_year_dict
 
 
 """
-Создаём функцию с SQL-запросом и выводим название, рейтинг и описание фильма.
+Создаём функцию детского рейтинга с SQL-запросом и выводим название, рейтинг и описание фильма.
 """
 
 
-def movies_rating():
+def movie_rating_children():
     with sqlite3.connect('netflix.db') as connection:
         cursor = connection.cursor()
 
-        querly = """
+        query = """
             SELECT title, rating, description
             FROM netflix
             WHERE "type" = 'Movie'
-            AND rating != ''
-            AND rating IS NOT NULL
+            AND rating = 'G'
             ORDER BY rating
         """
 
-        result_all = cursor.execute(querly)
+        cursor.execute(query)
+        result_all = cursor.fetchall()
+
         """
-        Прописываем список ключей, которые нам нужны.
+        Создаём пустой список, который будет содержать в себе словари.
         """
-        keys = [
-            "title",
-            "rating",
-            "description"
-        ]
-        """
-        Создаём пустой список, который будет в себе содержать ключи и значения.
-        """
+
         result = []
+
         """
         С помощью циклов for добавляем необходимые значения к списку ключей
         и добавляем это в список result.
         """
-        for result_row in result_all:
-            result.append({keys[i]: result_row[i] for i in range(len(keys))})
+
+        for row in result_all:
+            rating = {
+                'title': row[0],
+                'rating': row[1],
+                'description': row[2]
+            }
+            result.append(rating)
 
         return result
 
 
 """
-Cоздаём функцию для вывода данных о фильме по его названию.
+Создаём функцию семейного рейтинга с SQL-запросом и выводим название, рейтинг и описание фильма.
 """
 
 
-def title_dict(title):
-    """
-    :param title: здесь указываем название фильма
-    :return: возвращаем данные фильма по названию
-    """
-    list_dict = search_by_name()
-    """
-    Создаём пустой словарь который будет содержать необходимые данные конкретного фильма.
-    """
-    result = {}
-    """
-    В цикле for проходимся по всем словарям в списке и добавляем в result по 1 словарю для
-    сравнения названия (title) со значениями словаря.
-    """
-    for d in list_dict:
-        result.update(d)
-        if title in result.values():
-            return result
+def movie_rating_family():
+    with sqlite3.connect('netflix.db') as connection:
+        cursor = connection.cursor()
+
+        query = """
+            SELECT title, rating, description
+            FROM netflix
+            WHERE "type" = 'Movie'
+            AND rating = 'G' OR rating = 'PG' OR rating = 'PG-13'
+            ORDER BY rating
+        """
+
+        cursor.execute(query)
+        result_all = cursor.fetchall()
+
+        """
+        Создаём пустой список, который будет содержать в себе словари.
+        """
+
+        result = []
+
+        """
+        С помощью циклов for добавляем необходимые значения к списку ключей
+        и добавляем это в список result.
+        """
+
+        for row in result_all:
+            rating = {
+                'title': row[0],
+                'rating': row[1],
+                'description': row[2]
+            }
+            result.append(rating)
+
+        return result
 
 
 """
-Создаём функцию для вывода списка фильмов по году выпуска.
+Создаём функцию взрослого рейтинга с SQL-запросом и выводим название, рейтинг и описание фильма.
 """
 
 
-def title_years(year_1, year_2):
-    """
-    :param year_1: в этой переменной указываем С какого года будет поиск
-    :param year_2: в этой переменной указываем ПО какой год будет поиск
-    :return: возвращаем список словарей с названием и датой выхода фильма
-    """
-    list_dict = search_by_release_year()
-    """
-    Создаём пустой словарь который будет содержать необходимые данные конкретного фильма.
-    """
-    result = {}
+def movie_rating_adult():
+    with sqlite3.connect('netflix.db') as connection:
+        cursor = connection.cursor()
 
-    for d in list_dict:
-        result.update(d)
-        if year_1 or year_2 in result.values():
-            return result
+        query = """
+            SELECT title, rating, description
+            FROM netflix
+            WHERE "type" = 'Movie'
+            AND rating = 'R' OR rating = 'NC-17'
+            ORDER BY rating
+        """
+
+        cursor.execute(query)
+        result_all = cursor.fetchall()
+
+        """
+        Создаём пустой список, который будет содержать в себе словари.
+        """
+
+        result = []
+
+        """
+        С помощью циклов for добавляем необходимые значения к списку ключей
+        и добавляем это в список result.
+        """
+
+        for row in result_all:
+            rating = {
+                'title': row[0],
+                'rating': row[1],
+                'description': row[2]
+            }
+            result.append(rating)
+
+        return result
 
 
+"""
+Создаём функцию поиска по жанрам с SQL-запросом и выводим название и описание фильма.
+"""
 
+
+def title_genre(genre):
+    with sqlite3.connect('netflix.db') as connection:
+        cursor = connection.cursor()
+
+        query = f"""
+            SELECT title, description
+            FROM netflix
+            WHERE listed_in LIKE '%{genre}%'
+            AND "type" = 'Movie'
+            ORDER BY release_year DESC
+            LIMIT 10
+            """
+
+        cursor.execute(query)
+        result_all = cursor.fetchall()
+
+        """
+        Создаём пустой список, который будет содержать в себе словари.
+        """
+
+        result = []
+
+        """
+        С помощью циклов for добавляем необходимые значения к списку ключей
+        и добавляем это в список result.
+        """
+
+        for row in result_all:
+            listed_in = {
+                'title': row[0],
+                'description': row[1]
+            }
+            result.append(listed_in)
+
+        return result
+
+
+"""
+Создаём функцию поиска по паре актёров с SQL-запросом и выводим результат.
+"""
+
+
+def get_two_cast(cast_1, cast_2):
+    with sqlite3.connect('netflix.db') as connection:
+        cursor = connection.cursor()
+
+        query = f"""
+            SELECT COUNT(netflix.cast), netflix.cast
+            FROM netflix
+            WHERE netflix.cast LIKE '%{cast_1}%' AND '%{cast_2}%'
+            AND "type" = 'Movie'
+            GROUP BY netflix.cast
+            """
+
+        cursor.execute(query)
+        result_all = cursor.fetchall()
+
+        return result_all
+
+
+"""
+Создаём функцию поиска названия по разным критериям с SQL-запросом 
+и выводим название.
+"""
+
+
+def search_title_by_criteria(type_movie, release_date, genre):
+    with sqlite3.connect('netflix.db') as connection:
+        cursor = connection.cursor()
+
+        query = f"""
+            SELECT title, description
+            FROM netflix
+            WHERE "type" = '%{type_movie}%'
+            AND release_year = '%{release_date}%'
+            AND listed_in LIKE '%{genre}%'
+            """
+
+        cursor.execute(query)
+        result_all = cursor.fetchall()
+
+        return result_all
